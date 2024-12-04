769167f1b522de8b3b4f29ef4bbe3b8c591f623d
UmarA1Faruq <umaralfaruq1424@gmail.com>
2024-12-04 20:37:05 +0700

    Update code

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -4,7 +4,7 @@ export default function AddPage() {
     return (
         <>
             <title>Tambah Data Mahasiswa</title>
-            <div className="grid grid-cols-10 gap-4">
+            <div className="grid grid-cols-10 gap-4 items-center">
                 <div className=''>NPM</div>
                 <div className='col-span-4'>
                     <input
@@ -20,7 +20,18 @@ export default function AddPage() {
                         className="input input-bordered input-primary w-full max-w-xs" />
                 </div>
                 <div className='col-start-1'>Prodi</div>
-                <div className='col-span-4'>6</div>
+                <div className='col-span-4'>
+                    <select className="select select-primary w-full max-w-xs">
+                        <option disabled selected>Pilih Prodi Anda</option>
+                        <option>Informatika</option>
+                        <option>Sistem Informasi</option>
+                        
+                    </select>
+                </div>
+                <div className='col-start-2 col-span-4'>
+                <button className="btn btn-success mr-5X w-20">Simpan</button>
+                <button className="btn btn-warning ml-5X w-20">Batal</button>
+                </div>
 
             </div>
 
