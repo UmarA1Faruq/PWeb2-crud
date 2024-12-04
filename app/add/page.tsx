b7ff55275a8a6cba185e5a9d08f924256f50e8de
UmarA1Faruq <umaralfaruq1424@gmail.com>
2024-12-04 20:18:45 +0700

    Update code

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,24 +1,29 @@
 import React from 'react'
 
 export default function AddPage() {
-  return (
-    <>
-        <title>Tambah Data Mahasiswa</title>
-        <div className="grid grid-cols-10 gap-4">
-            <div className=''>NPM</div>
-            <div className='bg-teal-400'>2</div>
-            <div className='bg-teal-400'>3</div>
-            <div className='bg-teal-400'>4</div>
-            <div className='bg-teal-400'>5</div>
-            <div className='bg-teal-400'>6</div>
-            <div className='bg-teal-400'>7</div>
-            <div className='bg-teal-400'>8</div>
-            <div className='bg-teal-400'>9</div>
-            <div className='bg-teal-400'>10</div>
-            <div className='bg-teal-400'>11</div>
-            <div className='bg-teal-400'>12</div>
-        </div>
-        
-    </>
-  )
+    return (
+        <>
+            <title>Tambah Data Mahasiswa</title>
+            <div className="grid grid-cols-10 gap-4">
+                <div className=''>NPM</div>
+                <div className='col-span-4'>
+                    <input
+                        type="text"
+                        placeholder="Type here"
+                        className="input input-bordered input-primary w-full max-w-xs" />
+                </div>
+                <div className='col-start-1'>Nama</div>
+                <div className='col-span-4'>
+                    <input
+                        type="text"
+                        placeholder="Type here"
+                        className="input input-bordered input-primary w-full max-w-xs" />
+                </div>
+                <div className='col-start-1'>Prodi</div>
+                <div className='col-span-4'>6</div>
+
+            </div>
+
+        </>
+    )
 }
